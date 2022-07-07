4a5b33b87fb5dd49de087844fb6d80fdf519fee9
Florian Schneider schneider.f@me.com <schneider.f@me.com>
2022-07-07 21:55:34 +0200

    scroll is working

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -9,3 +9,8 @@ eagerLoadControllersFrom("controllers", application)
 // Lazy load controllers as they appear in the DOM (remember not to preload controllers in import map!)
 // import { lazyLoadControllersFrom } from "@hotwired/stimulus-loading"
 // lazyLoadControllersFrom("controllers", application)
+import { Application } from '@hotwired/stimulus'
+import ScrollTo from 'stimulus-scroll-to'
+
+const application = Application.start()
+application.register('scroll-to', ScrollTo)
